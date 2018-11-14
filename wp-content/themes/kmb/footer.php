080c674e51ee8d7d8f9308bd00164c344ca58139
programmer-man <bryan@kerigan.com>
2018-11-14 10:27:15 -0500

    footer update

--- wp-content/themes/kmb/footer.php
+++ wp-content/themes/kmb/footer.php
@@ -14,7 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info" style="text-align:center; font-size: .9em; color: #999;">
+		<div class="site-info" style="text-align:center; font-size: .9em; color: #999; padding: 0 1rem 2rem;">
 			<p>&copy; <?php echo date('Y'); ?> Mexico Beach Recovery.</p>
 			<p>Site by <a href="https://keriganmarketing.com" target="_blank" >Kerigan Marketing Associates</a> of Mexico Beach, FL.</p>
 		</div><!-- .site-info -->
