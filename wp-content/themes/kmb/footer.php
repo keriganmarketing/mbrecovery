4bd23d9a45cdc05bbd72fde17505ec618e1ff564
Bryan Baird <bryan@kerigan.com>
2018-11-26 11:47:32 -0500

    add tilt to footer

--- wp-content/themes/kmb/footer.php
+++ wp-content/themes/kmb/footer.php
@@ -16,7 +16,7 @@
 	<footer id="colophon" class="site-footer">
 		<div class="site-info" style="text-align:center; font-size: .9em; color: #999; padding: 0 1rem 2rem;">
 			<p>&copy; <?php echo date('Y'); ?> Mexico Beach Recovery.</p>
-			<p>Site by <a href="https://keriganmarketing.com" target="_blank" >Kerigan Marketing Associates</a> of Mexico Beach, FL.</p>
+			<p>A collaboration by <a href="https://tiltbuilt.com" target="_blank" >TILT</a> and <a href="https://keriganmarketing.com" target="_blank" >Kerigan Marketing Associates</a> of Mexico Beach, FL.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
